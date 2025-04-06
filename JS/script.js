4f136b6b1d0c33ceaba6d3bce9c0a2b1f6849a37
David Gutierrez <dagd0918@gmail.com>
2025-04-06 07:09:16 -0400

    SOlving some minor error, like the correcting the position of some elements

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+//================== General functions ==============================
+
 let menu = document.querySelector(".nav-links");
 function toggleMenu() {
   menu.classList.toggle("active");
@@ -15,10 +17,16 @@ function closeMenuOnClickOutside(event) {
   // Check if the click is NOT inside the menu or the hamburger button
   if (!menu.contains(event.target) && !button.contains(event.target)) {
     menu.classList.remove("active");
-    /* menu.classList.add("not-active"); */ //============================ I nedd to put a counter here!!!!!
+    menu.classList.add("not-active");
+    setTimeout(() => {
+      menu.classList.remove("not-active");
+    }, 200);
+    //============================ I nedd to put a counter here!!!!!
     document.removeEventListener("click", closeMenuOnClickOutside);
   }
 }
 
 // Optional: mobile touch support (in case of touch events)
 document.addEventListener("touchstart", closeMenuOnClickOutside);
+
+//========================= Destination only ==========================
